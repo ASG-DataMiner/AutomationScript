5afe58d1097439a806bc52ffa258bbe39ba117bf
jheir <jheir.design@gmail.com>
2023-03-28 07:46:06 -0700

    update

---
+++ EmpowerDemo2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerDemo2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerDemo2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A548B5EE-C8E0-4B82-817E-CC711E50795A")]
+[assembly: AssemblyVersion("1.0.0.0")]
